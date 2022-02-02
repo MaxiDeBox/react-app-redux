74de6c7636ff8abce0b7a753a4785e3451ed2068
Maxim <maxidebox@list.ru>
2022-02-02 09:33:57 +0400

    91. Как работает Redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    counter: 0
+  }
+
+  updateCounter(value) {
+    this.setState({
+      counter: this.state.counter + value
+    })
+  }
+
+  render() {
+    return (
+      <div className={'App'}>
+        <h1>Счетчик <strong>{this.state.counter}</strong></h1>
+
+        <hr/>
+
+        <div className="Actions">
+          <button onClick={() => this.updateCounter(1)}>Добавить 1</button>
+          <button onClick={() => this.updateCounter(-1)}>Вычесть 1</button>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
